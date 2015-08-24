2060f80746a028eab405d92a15430b40aa55dd87
Matthew Herron <matthew.herron77@gmail.com>
2015-08-24 09:29:29 -0500

    got rid of sqllite3 gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root "trains#show", page: 'index'
-  get "trains/train" => "trains#show"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
